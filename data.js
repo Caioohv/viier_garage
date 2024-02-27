c96c91d515940f01ee0361c393bab22469ccd1c7
Caio Vieira <caioviier@gmail.com>
2024-02-27 19:43:24 -0300

    feat: inclui lampadas na lista

---
+++ data.js
@@ -0,0 +1,109 @@
+module.exports = {
+  lamps: [
+    {
+      name: `Aileo - Z7 (A que uso!)`,
+      instructions: `Serve somente em faróis foco simples (que usam lâmpada H4). Ao comprar, certifique-se de escolher as opções "2pcs Z7" e "Left Hand Drive". Pode ser necessário remover o defletor do farol para instalar.`,
+      applications: ['H4'],
+      category: ['Farol', 'Mini Projetor'],
+      link: 'https://s.click.aliexpress.com/e/_mtiob3m'
+    },
+    {
+      name: `Aileo - Z6 (com opção 4300k)`,
+      instructions: `Serve somente em faróis foco simples (que usam lâmpada H4). Ao comprar, certifique-se de escolher as opções "Z6 2pcs" e "Left Hand Drive". Pode ser necessário remover o defletor do farol para instalar.`,
+      applications: ['H4'],
+      category: ['Farol', 'Mini Projetor'],
+      link: 'https://s.click.aliexpress.com/e/_mKQxxqe'
+    },
+    {
+      name: `Aileo - Projetor`,
+      instructions: `Certifique-se de escolher a opção "Left Hand Drive" e o soquete correto do seu carro. Pode ser necessário remover o defletor do farol para instalar.`,
+      applications: ['H8', 'H9', 'H11', 'H7', 'HB3', 'HB4'],
+      category: ['Farol', 'Mini Projetor'],
+      link: 'https://s.click.aliexpress.com/e/_m0tFmbW'
+    },
+    {
+      name: `DawnKnight - Projetor`,
+      instructions: `Certifique-se de escolher a opção "Left Hand Drive" e o soquete correto do seu carro. Pode ser necessário remover o defletor do farol para instalar.`,
+      applications: ['H8', 'H9', 'H11', 'H7', 'HB3'],
+      category: ['Farol', 'Mini Projetor'],
+      link: 'https://s.click.aliexpress.com/e/_mNTDq5Q'
+    },
+    {
+      name: `DawnKnight - K5C Plus Barata`,
+      instructions: `Certifique-se de escolher o soquete correto do seu carro.`,
+      applications: ['H1', 'H11', 'H7', 'HB3', 'H4'],
+      category: ['Farol', 'Lampada'],
+      link: 'https://s.click.aliexpress.com/e/_mPBQXIM'
+    },
+    {
+      name: `DawnKnight - K5C Plus`,
+      instructions: `Certifique-se de escolher o soquete correto do seu carro.`,
+      applications: ['H1', 'HIR2', '9012', 'D1', 'D2', 'D3', 'D4', 'H1', 'H11', 'H7', 'HB3', 'HB4', 'H4'],
+      category: ['Farol', 'Lampada'],
+      link: 'https://s.click.aliexpress.com/e/_mt3vj68'
+    },
+    {
+      name: `DawnKnight - K7C`,
+      instructions: `Certifique-se de escolher o soquete correto do seu carro.`,
+      applications: ['H1', 'HIR2', '9012', 'D1', 'D2', 'D3', 'D4', 'H1', 'H11', 'H7', 'HB3', 'HB4', 'H4'],
+      category: ['Farol', 'Lampada'],
+      link: 'https://s.click.aliexpress.com/e/_mt3vj68'
+    },
+    {
+      name: `DawnKnight - K7C`,
+      instructions: `Certifique-se de escolher o soquete correto do seu carro.`,
+      applications: ['H1', 'HIR2', 'D1', 'D2', 'D3', 'D4', 'H1', 'H11', 'H7', 'HB3', 'HB4', 'H4'],
+      category: ['Farol', 'Lampada'],
+      link: 'https://s.click.aliexpress.com/e/_mt3vj68'
+    },
+    {
+      name: `DawnKnight - K8C`,
+      instructions: `Certifique-se de escolher o soquete correto do seu carro.`,
+      applications: ['H1', 'HIR2', 'D1', 'D2', 'D3', 'D4', 'H1', 'H11', 'H7', 'HB3', 'HB4', 'H4'],
+      category: ['Farol', 'Lampada'],
+      link: 'https://s.click.aliexpress.com/e/_mt3vj68' //! trocar link
+    },
+    {
+      name: `Bevinsee - Z22`,
+      instructions: `Certifique-se de escolher o soquete correto do seu carro.`,
+      applications: ['H7'],
+      category: ['Farol', 'Lampada'],
+      link: 'https://s.click.aliexpress.com/e/_m0zsFsC'
+    },
+    {
+      name: `Bevinsee - F31`,
+      instructions: `Certifique-se de escolher a opção "6000k White" e o soquete correto do seu carro.`,
+      applications: ['HIR2', 'H8', 'H9', 'H11', 'H1', 'H3', 'H7', 'HB1', 'HB5', 'H13', 'HB3', 'HB2', 'H4', 'HB4', '880', '881', '896'],
+      category: ['Farol', 'Lampada', 'Milha', 'Neblina'],
+      link: 'https://s.click.aliexpress.com/e/_m0zsFsC'
+    },
+    {
+      name: `Auxito - H1`,
+      instructions: `Certifique-se de escolher a cor e o soquete correto do seu carro.`,
+      applications: ['H1'],
+      category: ['Farol', 'Lampada', 'Milha', 'Neblina'],
+      link: 'https://s.click.aliexpress.com/e/_mtvNHZS'
+    },
+    {
+      name: `Auxito - H3`,
+      instructions: `Certifique-se de escolher a cor e o soquete correto do seu carro.`,
+      applications: ['H3'],
+      category: ['Farol', 'Lampada', 'Milha', 'Neblina'],
+      link: 'https://s.click.aliexpress.com/e/_mKsBT3Q'
+    },
+    {
+      name: `Auxito - Mini Led`,
+      instructions: `Certifique-se de escolher o soquete correto do seu carro.`,
+      applications: ['HIR2', 'H8', 'H9', 'H11', 'H7', 'H16', 'HB3'],
+      category: ['Farol', 'Lampada', 'Milha', 'Neblina'],
+      link: 'https://s.click.aliexpress.com/e/_mKsBT3Q'
+    },
+    {
+      name: `Aslent - Ré, seta,...`,
+      instructions: `Certifique-se de escolher o soquete correto do seu carro e a cor da lâmpada.`,
+      applications: ['P21W', 'PY21W', 'P21/5W', 'T20 WY21W', 'T20 WY21/5W', 'T25 P27/7W'],
+      category: ['Lanterna', 'Lampada', 'Iluminação', 'Seta', 'Ré', 'Freio'],
+      link: 'https://s.click.aliexpress.com/e/_mOXlq8g'
+    },
+  ]
+}
